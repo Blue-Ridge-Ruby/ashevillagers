8d48758e8d996bb2b0056ae1ecff1fcbb382bbdc
Joshua Paine <joshua@letterblock.com>
2026-03-12 20:56:51 -0400

    test: build assets up front to avoid races

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,10 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+# Build Vite assets once in the main process before parallel workers fork,
+# so no worker races to auto-build the manifest.
+ViteRuby.commands.build
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers

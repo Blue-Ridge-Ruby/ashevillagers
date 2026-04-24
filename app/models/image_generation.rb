cab6277a7aef9c0b41661e4d425b5b22b8529a22
Joshua Paine <joshua@letterblock.com>
2026-04-23 22:29:50 -0400

    inage generation generates images!

--- app/models/image_generation.rb
+++ app/models/image_generation.rb
@@ -14,6 +14,7 @@ class ImageGeneration < ApplicationRecord
 
   def prompt
     attributes["prompt"].presence || begin
+      raise "Missing villager's job" unless job.present?
       self.prompt = p = (prompt_template % {animal:, job:})
       save if persisted?
       p
@@ -25,6 +26,27 @@ class ImageGeneration < ApplicationRecord
       self.animal = a = (self.class.animals - (profile&.image_generations&.pluck(:animal) || [])).sample
       save if persisted?
       a
+    end
+  end
+
+  def ensure_image!
+    return image if image.present?
+    raise "Missing source photo" unless source_photo.present?
+    raise "Missing prompt_for_image_generation configuration" unless prompt.present?
+    painted = Configured.RubyLLM.paint(prompt, model:, with: source_photo.blob)
+    @painted = painted if Rails.env.local? # for inspection in dev console
+    image.attach(
+      io: StringIO.new(painted.to_blob),
+      filename: "profile.png",
+      content_type: painted.mime_type,
+      identify: false
+    )
+  end
+
+  if Rails.env.development?
+    def preview!
+      raise "Missing image" unless image.blob.present?
+      system("open", "-a", "Preview", image.blob.service.path_for(image.blob.key))
     end
   end
 

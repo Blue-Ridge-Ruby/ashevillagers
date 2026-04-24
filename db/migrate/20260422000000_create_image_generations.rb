1317ef1ab61c1cfb40e0a3fbb93c078920e6a167
Joshua Paine <joshua@letterblock.com>
2026-04-23 23:19:19 -0400

    lint

--- db/migrate/20260422000000_create_image_generations.rb
+++ db/migrate/20260422000000_create_image_generations.rb
@@ -1,7 +1,7 @@
 class CreateImageGenerations < ActiveRecord::Migration[8.1]
   def change
     create_table :image_generations do |t|
-      t.references :profile_answer, null: false, foreign_key: true, index: { unique: true }
+      t.references :profile_answer, null: false, foreign_key: true, index: {unique: true}
       t.string :animal
       t.boolean :cursed, default: false, null: false
       t.text :prompt

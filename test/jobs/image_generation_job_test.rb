b43dbf87d294e90f76a2cc7d87c6fc43e7e65407
Joshua Paine <joshua@letterblock.com>
2026-04-26 21:59:12 -0400

    update test for ensure_cropped!

--- test/jobs/image_generation_job_test.rb
+++ test/jobs/image_generation_job_test.rb
@@ -1,14 +1,16 @@
 require "test_helper"
 
 class ImageGenerationJobTest < ActiveJob::TestCase
-  test "calls ensure_image! and broadcasts replace" do
+  test "calls ensure_image!, ensure_cropped! and broadcasts replace" do
     answer = profile_answers(:one_sport)
     ig = answer.create_image_generation!(animal: "fox", prompt: "stub")
 
     ensure_image_called = false
+    ensure_cropped_called = false
     broadcast_called_with = nil
 
     ig.define_singleton_method(:ensure_image!) { ensure_image_called = true }
+    ig.define_singleton_method(:ensure_cropped!) { ensure_cropped_called = true }
     ig.define_singleton_method(:broadcast_replace_to) do |*args, **opts|
       broadcast_called_with = {args: args, opts: opts}
     end
@@ -16,6 +18,7 @@ class ImageGenerationJobTest < ActiveJob::TestCase
     ImageGenerationJob.perform_now(ig)
 
     assert ensure_image_called
+    assert ensure_cropped_called
     assert_equal [ig.profile], broadcast_called_with[:args]
     assert_equal ActionView::RecordIdentifier.dom_id(ig), broadcast_called_with[:opts][:target]
     assert_equal "profiles/image_generation", broadcast_called_with[:opts][:partial]
